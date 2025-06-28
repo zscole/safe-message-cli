fef555f93da842c6fcb985c862ef6c8deeeef1c1
zsole <zcole@linux.com>
2025-06-28 11:54:04 -0500

    fix

--- safe-app/src/main.tsx
+++ safe-app/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SafeProvider } from '@safe-global/safe-apps-react-sdk'
+import SafeAppProvider from '@safe-global/safe-apps-react-sdk'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SafeProvider>
+    <SafeAppProvider>
       <App />
-    </SafeProvider>
+    </SafeAppProvider>
   </StrictMode>,
 )
